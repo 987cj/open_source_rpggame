a4da6d885c8a7b7bfbef8d8b62d30576229d8f87
cmartyr <cjoycemartyr@gmail.com>
2026-03-12 17:25:07 +1030

    feat: added basic grid, game_grid scene will generate xy grid

---
+++ nordic-conquest/game_grid.gd
@@ -0,0 +1,25 @@
+class_name GameGrid extends PanelContainer
+
+@export var grid_coords: Vector2i
+@export var grid_box: PackedScene
+
+const TILE_PIXELS = 64
+
+var grid_items = Vector2i(10, 10)
+
+@onready var grid = get_node("GridContainer")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	grid.columns = grid_coords.x
+	self.custom_minimum_size = grid_coords * TILE_PIXELS
+	initialise_grid()
+
+
+func initialise_grid() -> void:
+	for row in grid_coords.x:
+		for col in grid_coords.y:
+			var box = grid_box.instantiate()
+			grid.add_child(box)
+			#box.global_position = Vector2(TILE_PIXELS * row, TILE_PIXELS * col)
+			#print(box.global_position)
